ea726d68bea8253469ee8c95e00fff0db674b79b
Kittyjuanes <114379404+Kittyjuanes@users.noreply.github.com>
2025-03-03 19:30:49 -0500

    Creacion de la Entidad "Usuario"

---
+++ src/main/java/com/example/proyecto/proyecto/entities/Usuario.java
@@ -0,0 +1,40 @@
+package com.example.proyecto.proyecto.entities;
+
+
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+
+import jakarta.annotation.Generated;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "usuario")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Where(clause = "status = 0")
+@SQLDelete( sql = "UPTADE usuario SET status = 1 WHERE id_usuario = ?")
+
+
+public class Usuario {
+
+    @Id
+    @Column(name = "id_usuario")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idUsuario;
+    private String nombre_Usuario;
+    private String direccion;
+    private Integer edad;
+
+    
+}
