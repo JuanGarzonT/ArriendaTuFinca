6a19102bf1d4388534e80db69ae71d94aadf0b06
lmendieta06 <laumendieta06@gmail.com>
2025-05-12 21:42:43 -0500

    Agregar CrossOrigin en todos los servicios

--- src/main/java/com/example/proyecto/proyecto/services/UsuarioService.java
+++ src/main/java/com/example/proyecto/proyecto/services/UsuarioService.java
@@ -17,9 +17,12 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.List;
 import java.util.stream.Collectors;
+@CrossOrigin("*")
+
 
 @Service
 public class UsuarioService {
